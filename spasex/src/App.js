8780f39e3317fed4e26a4769ca152ce55f2e6ca2
MakarPustovalov <makar.pht@gmail.com>
2020-10-14 18:45:46 +0300

    homework

--- spasex/src/App.js
+++ spasex/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './style.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Features />
+      <Footer />
     </>
   );
 }
